79bd9cc5d8a97d3921aea4c0cb95801ebcd30f97
wjx8382 <838275760@qq.com>
2023-04-24 10:32:06 +0800

    添加Dockerfile

--- backend/main.go
+++ backend/main.go
@@ -1,69 +1,69 @@
 package main
 
 import (
-	"flag"
-	"fmt"
-	"log"
-	"net/http"
+        "flag"
+        "fmt"
+        "log"
+        "net/http"
 
-	"github.com/gorilla/mux"
-	"k8s.io/client-go/rest"
-	"k8s.io/client-go/tools/clientcmd"
-	"k8s.io/client-go/util/homedir"
+        "github.com/gorilla/mux"
+        "k8s.io/client-go/rest"
+        "k8s.io/client-go/tools/clientcmd"
+        "k8s.io/client-go/util/homedir"
 
-	"OpsWorks/api"
+        "OpsWorks/api"
 )
 
 func main() {
-	var kubeconfig string
-	if home := homedir.HomeDir(); home != "" {
-		flag.StringVar(&kubeconfig, "kubeconfig", fmt.Sprintf("%s/.kube/config", home), "(optional) absolute path to the kubeconfig file")
-	} else {
-		flag.StringVar(&kubeconfig, "kubeconfig", "", "absolute path to the kubeconfig file")
-	}
-	flag.Parse()
+        var kubeconfig string
+        if home := homedir.HomeDir(); home != "" {
+                flag.StringVar(&kubeconfig, "kubeconfig", fmt.Sprintf("%s/.kube/config", home), "(optional) absolute path to the kubeconfig file")
+        } else {
+                flag.StringVar(&kubeconfig, "kubeconfig", "", "absolute path to the kubeconfig file")
+        }
+        flag.Parse()
 
-	// Build config from kubeconfig file or in-cluster config
-	config, err := buildConfig(kubeconfig)
-	if err != nil {
-		log.Fatal(err)
-	}
+        // Build config from kubeconfig file or in-cluster config
+        config, err := buildConfig(kubeconfig)
+        if err != nil {
+                log.Fatal(err)
+        }
 
-	// Create router
-	router := mux.NewRouter()
+        // Create router
+        router := mux.NewRouter()
 
-	// Define routes
-	router.HandleFunc("/pods", func(w http.ResponseWriter, r *http.Request) {
-		names, err := api.GetPodList(config)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
+        // Define routes
+        router.HandleFunc("/pods", func(w http.ResponseWriter, r *http.Request) {
+                names, err := api.GetPodList(config)
+                if err != nil {
+                        http.Error(w, err.Error(), http.StatusInternalServerError)
+                        return
+                }
 
-		for _, name := range names {
-			fmt.Fprintf(w, "Pod: %s\n", name)
-		}
-	})
+                for _, name := range names {
+                        fmt.Fprintf(w, "Pod: %s\n", name)
+                }
+        })
 
-	// Start server
-	log.Fatal(http.ListenAndServe(":8080", router))
+        // Start server
+        log.Fatal(http.ListenAndServe("0.0.0.0:8080", router))
 }
 
 func buildConfig(kubeconfig string) (*rest.Config, error) {
 	fmt.Println(kubeconfig)
 	if kubeconfig == "" {
-		fmt.Println("InClusterConfig")
-		return rest.InClusterConfig()
+			fmt.Println("InClusterConfig")
+			return rest.InClusterConfig()
 	}
 
 	kubeConfig, err := clientcmd.LoadFromFile(kubeconfig)
 	if err != nil {
-		return nil, err
+			return nil, err
 	}
 
 	restConfig, err := clientcmd.NewDefaultClientConfig(*kubeConfig, &clientcmd.ConfigOverrides{}).ClientConfig()
 	if err != nil {
-		return nil, err
+			return nil, err
 	}
 
 	return restConfig, nil
